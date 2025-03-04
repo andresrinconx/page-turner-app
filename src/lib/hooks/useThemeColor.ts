8876edd31b04ce7f584a9478646fb8b61d8b84c4
Andres Rincon <andresrinconx@gmail.com>
2025-03-04 19:42:57 -0400

    cleaning and libraries installed

--- src/lib/hooks/useThemeColor.ts
+++ src/lib/hooks/useThemeColor.ts
@@ -1,16 +1,12 @@
-/**
- * Learn more about light and dark modes:
- * https://docs.expo.dev/guides/color-schemes/
- */
-
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+/* eslint-disable prettier/prettier */
+import { Colors } from "@/src/lib/constants";
+import { useColorScheme } from "@/src/lib/hooks/useColorScheme";
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
 ) {
-  const theme = useColorScheme() ?? 'light';
+  const theme = useColorScheme() ?? "light";
   const colorFromProps = props[theme];
 
   if (colorFromProps) {
